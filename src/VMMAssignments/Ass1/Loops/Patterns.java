fec52c54c1d40d034bdf299dc4d449cc5a7168c0
jai11k <jai11simran@gmail.com>
2020-06-25 20:49:13 +0530

    Patterns.java file modified

--- src/VMMAssignments/Ass1/Loops/Patterns.java
+++ src/VMMAssignments/Ass1/Loops/Patterns.java
@@ -4,72 +4,40 @@ public class Patterns
 {
     public static void main(String[] args)
     {
-//        pattern 1           1 2 3 4 5
+//        pattern1();
 
-//          int i,j;
-//         for (i=1;i<=5;i++)
-//         {
-//             for (j=1;j<=i;j++)
-//
-//
-//                 System.out.print(""+j);
-//
-//             System.out.print("\n");
-//         }
+        System.out.print("\n");
+        System.out.print("\n");
 
-//      pattern 1 22  333 4444 55555
+//        pattern2();
 
-//
-//         for (int i =1;i<=10;i++)
-//        {
-//            for (int j=1; j<=i; j++)
-//
-//
-//                System.out.print(" " +i) ;
-//
-//                System.out.print("\n");
-//
-//        }
-//         for(int i=10-1;i>0;i--)
-//         {
-//             for (int j=i;j>0;j--)
-//
-//
-//                 System.out.print(" " +i) ;
-//
-//                 System.out.print("\n");
-//
-//             }
+        System.out.print("\n");
+        System.out.print("\n");
 
+   //     pattern3();
 
-        // patern of stars
+        System.out.print("\n");
+        System.out.print("\n");
 
+        pattern4();
 
-//        int i,j ;
-//
-//        for (i=1; i<6 ;i++)
-//        {
-//            for (j=1;j<=i;j++)
-//
-//                System.out.print("+");
-//
-//            System.out.print("\n");
-//        }
+        System.out.print("\n");
+        System.out.print("\n");
 
-//        pattern  5 54 543 5432 54321
-
-
-//        int i,j;
-//        for (i=5;i>=1;i--)
-//         {
-//           for (j=5;j>=i;j--)
-//
-//                System.out.print(""+j);
-//         System.out.print("\n");
-//         }
+    //    pattern5();
 
 
 
+    }
+
+    private static void pattern1()
+    {
+
+//        55555
+//        4444
+//        333
+//        22
+//        1
         int i,j;
         for (i=6-1;i>0;i--)
         {
@@ -78,10 +46,81 @@ public class Patterns
                 System.out.print(""+i);
             System.out.print("\n");
         }
+    }
+
+    private static  void pattern2()
+    {
+    //        5
+//        54
+//        543
+//        5432
+//        54321
+
+    int i,j;
+    for (i=5;i>=1;i--)
+    {
+        for (j=5;j>=i;j--)
+
+            System.out.print(""+j);
+        System.out.print("\n");
+    }
+
+}
+
+    private static void pattern3()
+    {
+//        1
+//        2 2
+//        3 3 3
+//        4 4 4 4
+//        5 5 5 5 5
+
+         for (int i =1;i<=5;i++)
+        {
+            for (int j=1; j<=i; j++)
 
 
+                System.out.print(" " +i) ;
+
+                System.out.print("\n");
+        }
+    }
+
+    private static void pattern4()
+    {
+
+//        5 5 5 5 5
+//        4 4 4 4
+//        3 3 3
+//        2 2
+//        1
 
 
+        for(int i=6-1;i>0;i--)
+         {
+             for (int j=i;j>0;j--)
+
+
+                 System.out.print(" " +i) ;
+
+                 System.out.print("\n");
+
+             }
+
+    }
+
+    private  static void pattern5()
+    {
+                int i,j ;
+
+        for (i=1; i<6 ;i++)
+        {
+            for (j=1;j<=i;j++)
+
+                System.out.print("+");
+
+            System.out.print("\n");
+        }
 
     }
 
