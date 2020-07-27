0d8f43a9878a80dfa4855c2e4e382bdf0c121043
TheKaranGhai <karanghai1932@gmail.com>
2020-07-27 12:26:51 +0530

    Synchronization

---
+++ src/Synchronization/NavinReddy/SynchronizationLearning.java
@@ -0,0 +1,50 @@
+package Synchronization.NavinReddy;
+   class Test
+   {
+       int count;
+       public  synchronized void increment()
+       {
+           count++;
+       }
+
+   }
+public class SynchronizationLearning
+{
+    public static void main(String[] args) throws InterruptedException {
+        Test t1=new Test();
+
+        Thread obj1 =new Thread(new Runnable()
+        {
+
+            @Override
+            public void run() {
+                for (int i = 1; i <=1000 ; i++)
+                {
+                           t1.increment();
+                }
+            }
+        });
+
+        obj1.start();
+        obj1.join();
+
+
+
+        Thread obj2=new Thread(new Runnable()
+        {
+
+            @Override
+            public void run() {
+                for (int i = 1; i <=1000 ; i++)
+                {
+                    t1.increment();
+                }
+            }
+        });
+
+
+       obj2.start();
+        obj2.join();
+        System.out.println("Count = " + t1.count);
+    }
+}
