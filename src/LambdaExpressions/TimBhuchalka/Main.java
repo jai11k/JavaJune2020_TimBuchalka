ce5c2c4cd314ed237dbed23040163a7a4e1dc077
jai11k <jai11simran@gmail.com>
2020-08-16 01:47:28 +0530

    first commit

--- src/LambdaExpressions/TimBhuchalka/Main.java
+++ src/LambdaExpressions/TimBhuchalka/Main.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 
 public class Main {
-
     public static void main(String[] args) {
         new Thread(()-> {
             System.out.println("Printing from the Runnable");
